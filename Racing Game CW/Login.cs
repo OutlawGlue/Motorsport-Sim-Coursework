35951a368be284cbb419e5b5457d672a32838c1b
Matthew Irvine <irvinematthew28@gmail.com>
2025-10-18 16:18:55 +0100

    Add project files.

---
+++ Racing Game CW/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Racing_Game_CW
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<User> ReturnUsers()
+        {
+            List<User> users = new List<User>();
+            using (StreamReader sr = new StreamReader("Users.txt"))
+            {
+
+            }
+
+
+            return users;
+        }
+    }
+}
---
+++ Racing Game CW/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racing_Game_CW
+{
+    internal class User
+    {
+        private string username;
+        private string password;
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
